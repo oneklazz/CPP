143a528bcd92c5f69c88c2085cd55ca0f8a912dc
onek1azz <andrelian999@gmai.com>
2026-05-30 22:52:23 +0800

    added main.cpp and Makefile

---
+++ exam/five-in-a-row/src/main.cpp
@@ -0,0 +1,17 @@
+#include "game/game.h"
+#include "players/human_player.h"
+#include "players/computer_player.h"
+#include "players/computer.h"
+
+// создает все, стратегию, игроков и запускает игру
+int main() {
+    computer strategy;
+    human_player human('X');
+    computer_player computer('O', strategy);
+
+    game game(human, computer, strategy);
+    game.run();
+
+    std::cout << "\nигра завершена, не забудьте сохранить историю, если еще не сделали.\n";
+    return 0;
+}
